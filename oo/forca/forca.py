7d7703f5f8141e2f9e6544307d21acf6c1d2bdf2
Klaus Kazlauskas <klausmachado@gmail.com>
2020-12-30 17:44:57 -0300

    refactor(oo): use private na forca

--- oo/forca/forca.py
+++ oo/forca/forca.py
@@ -4,54 +4,54 @@ from jogos.helpers import map_positions, normalize, sleep, clear
 
 
 class Forca:
-    _won: bool
-    _lost: bool
-    _tries: int
-    _guessed_letters: list
-    _mapped_word_positions: dict
-    _hidden_word: list
+    __won: bool
+    __lost: bool
+    __tries: int
+    __guessed_letters: list
+    __mapped_word_positions: dict
+    __hidden_word: list
 
-    PLACEHOLDER_LETTER = '_'
+    __PLACEHOLDER_LETTER = '_'
 
     def __init__(self):
-        self._secret_word: str = ''
-        self._gallows_draw: list = []
+        self.___secret_word: str = ''
+        self.__gallows_draw: list = []
         with open('defeat_message.txt', 'r') as defeat_file:
-            self._defeat_message = [line for line in defeat_file]
-            self._defeat_message = ''.join(self._defeat_message)
+            self.__defeat_message = [line for line in defeat_file]
+            self.__defeat_message = ''.join(self.__defeat_message)
 
         with open('victory_message.txt', 'r') as victory_file:
-            self._victory_message = [line for line in victory_file]
-            self._victory_message = ''.join(self._victory_message)
+            self.__victory_message = [line for line in victory_file]
+            self.__victory_message = ''.join(self.__victory_message)
 
 
 
     def start_new_game(self):
-        self._reset_states()
-        self._print_opening_message()
+        self.__reset_states()
+        self.__print_opening_message()
 
-        while(not self._won and not self._lost):
-            self._next_round()
+        while(not self.__won and not self.__lost):
+            self.__next_round()
 
-        self._end_game()
+        self.__end_game()
 
     @property
-    def secret_word(self):
-        return self._secret_word
+    def __secret_word(self):
+        return self.___secret_word
 
-    @secret_word.setter
-    def secret_word(self, secret_word):
+    @__secret_word.setter
+    def __secret_word(self, secret_word):
         secret_word = normalize(secret_word)
-        self._mapped_word_positions = map_positions(secret_word)
-        self._hidden_word = [Forca.PLACEHOLDER_LETTER for _ in secret_word]
-        self._secret_word = secret_word
+        self.__mapped_word_positions = map_positions(secret_word)
+        self.__hidden_word = [Forca.__PLACEHOLDER_LETTER for _ in secret_word]
+        self.___secret_word = secret_word
 
-    def _reset_states(self):
-        self._won = False
-        self._lost = False
-        self._tries = 7
-        self._guessed_letters = []
-        self._gallows_draw = [
+    def __reset_states(self):
+        self.__won = False
+        self.__lost = False
+        self.__tries = 7
+        self.__guessed_letters = []
+        self.__gallows_draw = [
             "  _______     ",
             " |/      |    ",
             " |            ",
@@ -62,18 +62,18 @@ class Forca:
             "_|___         ",
             ""
         ]
-        self.set_random_secret_word()
+        self.__set_random_secret_word()
 
-    def set_random_secret_word(self):
-        self.secret_word = self.get_random_secret_word()
+    def __set_random_secret_word(self):
+        self.__secret_word = self.__get_random_secret_word()
 
-    def get_random_secret_word(self, file_path='fruits.txt'):
+    def __get_random_secret_word(self, file_path='fruits.txt'):
         with open(file_path, 'r', encoding='utf-8') as file:
             words = [line for line in file]
 
         return words[randint(0, len(words) - 1)]
 
-    def _print_opening_message(self):
+    def __print_opening_message(self):
         clear()
         print('***************************')
         print("Bem vindo ao jogo da Forca!")
@@ -82,43 +82,43 @@ class Forca:
         print()
         input("Aperte enter para começar")
 
-    def _next_round(self):
-        self.print_round_start_message()
-        guess, guessed_before = self.input_guess()
+    def __next_round(self):
+        self.__print_round_start_message()
+        guess, guessed_before = self.__input_guess()
 
         if (guessed_before):
             return
 
-        self.check_guess(guess)
+        self.__check_guess(guess)
 
-        self._won = Forca.PLACEHOLDER_LETTER not in self._hidden_word
-        self._lost = self._tries <= 0
+        self.__won = Forca.__PLACEHOLDER_LETTER not in self.__hidden_word
+        self.__lost = self.__tries <= 0
 
-    def _end_game(self):
+    def __end_game(self):
         clear()
-        if (self._won):
-            self._print_victory_message()
-        elif (self._lost):
-            self._print_defeat_message()
+        if (self.__won):
+            self.__print_victory_message()
+        elif (self.__lost):
+            self.__print_defeat_message()
 
-    def print_round_start_message(self):
+    def __print_round_start_message(self):
         clear()
         print('A palavra secreta é:')
-        print(' '.join(self._hidden_word))
+        print(' '.join(self.__hidden_word))
         print()
-        print(f"Você tem {self._tries} tentativas.")
+        print(f"Você tem {self.__tries} tentativas.")
         print()
-        self._draw_gallows()
+        self.__draw_gallows()
         print()
 
-    def input_guess(self):
+    def __input_guess(self):
         guess = input("Chute uma letra: ")
         guess = normalize(guess)
-        guessed_before = guess in self._guessed_letters
+        guessed_before = guess in self.__guessed_letters
 
         if (not guessed_before):
             print(f"Você chutou '{guess}'")
-            self._guessed_letters.append(guess)
+            self.__guessed_letters.append(guess)
             sleep(0.6)
         else:
             print(f"A letra '{guess}' já foi chutada anteriormente")
@@ -126,55 +126,55 @@ class Forca:
 
         return guess, guessed_before
 
-    def check_guess(self, guess):
-        if (guess in self._mapped_word_positions):
-            indexes = self._mapped_word_positions[guess]
+    def __check_guess(self, guess):
+        if (guess in self.__mapped_word_positions):
+            indexes = self.__mapped_word_positions[guess]
             letter_count = len(indexes)
             print(f"Tem {letter_count} letras '{guess}'")
 
             for i in indexes:
-                self._hidden_word[i] = guess
+                self.__hidden_word[i] = guess
         else:
             print("Não foi dessa vez.")
-            self._tries -= 1
+            self.__tries -= 1
 
         sleep(2)
 
-    def _print_victory_message(self):
-        print(self._victory_message)
+    def __print_victory_message(self):
+        print(self.__victory_message)
 
-    def _print_defeat_message(self):
-        params = {'secret_word': self.secret_word}
-        print(self._defeat_message.format(**params))
+    def __print_defeat_message(self):
+        params = {'secret_word': self.__secret_word}
+        print(self.__defeat_message.format(**params))
 
-    def _draw_gallows(self):
+    def __draw_gallows(self):
         head_position = 2
         body_position = 3
         lower_body_position = 4
         legs_position = 5
 
-        if (self._tries == 6):
-            self._gallows_draw[head_position] = " |      (_)   "
+        if (self.__tries == 6):
+            self.__gallows_draw[head_position] = " |      (_)   "
 
-        if (self._tries == 5):
-            self._gallows_draw[body_position] = " |      \     "
+        if (self.__tries == 5):
+            self.__gallows_draw[body_position] = " |      \     "
 
-        if (self._tries == 4):
-            self._gallows_draw[body_position] = " |      \|    "
+        if (self.__tries == 4):
+            self.__gallows_draw[body_position] = " |      \|    "
 
-        if (self._tries == 3):
-            self._gallows_draw[body_position] = " |      \|/   "
+        if (self.__tries == 3):
+            self.__gallows_draw[body_position] = " |      \|/   "
 
-        if (self._tries == 2):
-            self._gallows_draw[lower_body_position] = " |       |    "
+        if (self.__tries == 2):
+            self.__gallows_draw[lower_body_position] = " |       |    "
 
-        if (self._tries == 1):
-            self._gallows_draw[legs_position] = " |      /     "
+        if (self.__tries == 1):
+            self.__gallows_draw[legs_position] = " |      /     "
 
-        if (self._tries == 0):
-            self._gallows_draw[legs_position] = " |      / \   "
+        if (self.__tries == 0):
+            self.__gallows_draw[legs_position] = " |      / \   "
 
-        print('\n'.join(self._gallows_draw))
+        print('\n'.join(self.__gallows_draw))
 
 f = Forca()
 f.start_new_game()
