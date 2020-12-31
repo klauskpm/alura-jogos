fecbae03008250c69d267224fbfda78b0d045bef
Klaus Kazlauskas <klausmachado@gmail.com>
2020-12-31 12:27:46 -0300

    feat(oo-avancado): criando atributos protegidos na classe de base

--- oo-avancado/model.py
+++ oo-avancado/model.py
@@ -3,9 +3,17 @@ from abc import ABC
 
 class VideoABC(ABC):
     def __init__(self, title, year):
-        self.title = title.title()
+        self.__title = title.title()
         self.year = year
         self.__likes = 0
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def like(self):
         self.__likes += 1
@@ -21,3 +29,11 @@ class Series(VideoABC):
     def __init__(self, title, year, seasons):
         super().__init__(title, year)
         self.seasons = seasons
+
+
+ving = Movie('vingadores: guerra infinita', 2018, 200)
+ving.like()
+ving.like()
+print(ving.likes)
+ving.likes = 7
+print(ving.likes)
