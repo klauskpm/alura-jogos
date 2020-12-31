c6a3453b5412acd9d3951ed4d3dfbb3abc67ab57
Klaus Kazlauskas <klausmachado@gmail.com>
2020-12-31 20:17:37 -0300

    refactor(oo-avancado): usa dunder de str para imprimir dados das classes

--- oo-avancado/model.py
+++ oo-avancado/model.py
@@ -7,6 +7,9 @@ class VideoABC(ABC):
         self.year = year
         self._likes = 0
 
+    def __str__(self):
+        return f'Título {self.title} - Ano {self.year} | likes {self.likes}'
+
     @property
     def title(self):
         return self._title
@@ -24,14 +27,28 @@ class Movie(VideoABC):
         super().__init__(title, year)
         self.duration = duration
 
+    def __str__(self):
+        return f'Título {self.title}' \
+               f' - Ano {self.year}' \
+               f' - Duração {self.duration} minutos' \
+               f' | likes {self.likes}'
+
 
 class Series(VideoABC):
     def __init__(self, title, year, seasons):
         super().__init__(title, year)
         self.seasons = seasons
 
+    def __str__(self):
+        return f'Título {self.title}' \
+               f' - Ano {self.year}' \
+               f' - {self.seasons} temporadas' \
+               f' | likes {self.likes}'
 
-ving = Series('vingadores: guerra infinita', 2018, 200)
+
+ving = Movie('vingadores: guerra infinita', 2018, 200)
 ving.like()
 ving.like()
 print(ving.likes)
+
+print(ving)
