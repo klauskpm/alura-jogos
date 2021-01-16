cbd3696cc5f3348a4f56216f6f6c937f9e1dca71
Klaus Kazlauskas <klausmachado@gmail.com>
2021-01-16 19:04:47 -0300

    refactor(stw): renomeia propriedades com duplo underline para unico

--- spin-the-wheel/Round.py
+++ spin-the-wheel/Round.py
@@ -6,7 +6,7 @@ from helpers import clear, sleep, normalize
 
 
 class Round:
-    __LETTER_VALUE = 100
+    _LETTER_VALUE = 100
 
     def __init__(self, word: str, players: Tuple[Player, ...] = None):
         if players is None:
@@ -16,11 +16,11 @@ class Round:
         self._current_player_index = 0
         self._current_player = self._players[0]
 
-        self.__guessed_word = False
-        self.__was_hanged = False
-        self.__tries = 7
-        self.__guessed_letters = []
-        self.__gallows_draw = [
+        self._guessed_word = False
+        self._was_hanged = False
+        self._tries = 7
+        self._guessed_letters = []
+        self._gallows_draw = [
             "  _______     ",
             " |/      |    ",
             " |            ",
@@ -37,19 +37,19 @@ class Round:
         return self._run_turn()
 
     def _run_turn(self):
-        self.__print_round_start_message()
-        guess = self.__input_guess()
+        self._print_round_start_message()
+        guess = self._input_guess()
 
         if self._has_guessed_before(guess):
             return self._run_turn()
 
-        self.__check_guess(guess)
+        self._check_guess(guess)
 
-        self.__guessed_word = self._secret_word.has_guessed_word()
-        self.__was_hanged = self.__tries <= 0
+        self._guessed_word = self._secret_word.has_guessed_word()
+        self._was_hanged = self._tries <= 0
 
-        if (self.__guessed_word or self.__was_hanged):
-            return self.__guessed_word
+        if (self._guessed_word or self._was_hanged):
+            return self._guessed_word
         else:
             return self._next_turn()
 
@@ -65,21 +65,21 @@ class Round:
         self._current_player_index = next_player_index
         self._current_player = self._players[next_player_index]
 
-    def __check_guess(self, guess):
+    def _check_guess(self, guess):
         letter_count = self._secret_word.get_letter_count(guess)
         if letter_count > 0:
             self._secret_word.guess_letter(guess)
-            earned_money = Round.__LETTER_VALUE * letter_count
+            earned_money = Round._LETTER_VALUE * letter_count
             print(f"Tem {letter_count} letras '{guess}'")
             print(f"Você ganhou R${earned_money:.2f}")
             self._current_player.add_money(earned_money)
         else:
             print("Não foi dessa vez.")
-            self.__tries -= 1
+            self._tries -= 1
 
         sleep(2)
 
-    def __print_round_start_message(self):
+    def _print_round_start_message(self):
         clear()
         print(f"Rodada de {self._current_player.name}")
         print(f"(R${self._current_player.money:.2f})")
@@ -87,23 +87,23 @@ class Round:
         print('A palavra secreta é:')
         print(' '.join(self._secret_word.get_hidden_word()))
         print()
-        print(f"Você tem {self.__tries} tentativas.")
+        print(f"Você tem {self._tries} tentativas.")
         print()
-        self.__draw_gallows()
+        self._draw_gallows()
         print()
 
-    def __input_guess(self):
+    def _input_guess(self):
         guess = input("Chute uma letra: ")
         guess = normalize(guess)
 
         return guess
 
     def _has_guessed_before(self, guess):
-        guessed_before = guess in self.__guessed_letters
+        guessed_before = guess in self._guessed_letters
 
         if (not guessed_before):
             print(f"Você chutou '{guess}'")
-            self.__guessed_letters.append(guess)
+            self._guessed_letters.append(guess)
             sleep(0.6)
         else:
             print(f"A letra '{guess}' já foi chutada anteriormente")
@@ -111,31 +111,31 @@ class Round:
 
         return guessed_before
 
-    def __draw_gallows(self):
+    def _draw_gallows(self):
         head_position = 2
         body_position = 3
         lower_body_position = 4
         legs_position = 5
 
-        if (self.__tries == 6):
-            self.__gallows_draw[head_position] = " |      (_)   "
+        if (self._tries == 6):
+            self._gallows_draw[head_position] = " |      (_)   "
 
-        if (self.__tries == 5):
-            self.__gallows_draw[body_position] = " |      \     "
+        if (self._tries == 5):
+            self._gallows_draw[body_position] = " |      \     "
 
-        if (self.__tries == 4):
-            self.__gallows_draw[body_position] = " |      \|    "
+        if (self._tries == 4):
+            self._gallows_draw[body_position] = " |      \|    "
 
-        if (self.__tries == 3):
-            self.__gallows_draw[body_position] = " |      \|/   "
+        if (self._tries == 3):
+            self._gallows_draw[body_position] = " |      \|/   "
 
-        if (self.__tries == 2):
-            self.__gallows_draw[lower_body_position] = " |       |    "
+        if (self._tries == 2):
+            self._gallows_draw[lower_body_position] = " |       |    "
 
-        if (self.__tries == 1):
-            self.__gallows_draw[legs_position] = " |      /     "
+        if (self._tries == 1):
+            self._gallows_draw[legs_position] = " |      /     "
 
-        if (self.__tries == 0):
-            self.__gallows_draw[legs_position] = " |      / \   "
+        if (self._tries == 0):
+            self._gallows_draw[legs_position] = " |      / \   "
 
-        print('\n'.join(self.__gallows_draw))
+        print('\n'.join(self._gallows_draw))
