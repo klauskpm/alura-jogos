d507caf51f50bd1cf4edf88575ca6b43bd094bc1
Klaus Kazlauskas <klausmachado@gmail.com>
2021-01-23 11:53:43 -0300

    fix(round): remova lógica que adiciona 5 a todos os decimais

--- round.py
+++ round.py
@@ -10,46 +10,40 @@ def round_to(value, decimals=0, strategy=ROUND_HALF_UP):
     return to_decimal(value).quantize(precision, strategy)
 
 
-def round_half_up(value, decimals=0):
+def round_half_to_positive(value, decimals=0):
     value = to_decimal(value)
+    rounding_strategy = ROUND_HALF_DOWN
     if value > 0:
-        value = _aux_round_up(value, decimals)
-    return round_to(value, decimals, ROUND_HALF_DOWN)
+        rounding_strategy = ROUND_HALF_UP
+    return round_to(value, decimals, rounding_strategy)
 
 
-def _aux_round_up(value, decimals):
-    multiplier = 10 ** decimals
-    multiplied_value = (value * multiplier).normalize()
-
-    _, extra_decimals = str(multiplied_value).split('.')
-    extra_decimals = len(extra_decimals)
-
-    aux_value = '0.' + ('5' * extra_decimals)
-    aux_value = Decimal(aux_value)
-
-    return (multiplied_value + aux_value).quantize(Decimal('1.'), ROUND_DOWN) / multiplier
+def compare_round(value, precision):
+    true_half_up = round_half_to_positive(value, precision)
+    python_half_up = round_to(value, precision)
+    print(f"{value} => {true_half_up} | {python_half_up}")
 
 
 print('1 decimal')
-print(round_half_up(1.23, 1), round_to(1.23, 1))
-print(round_half_up(1.25, 1), round_to(1.25, 1))
-print(round_half_up(1.235, 1), round_to(1.235, 1))
-print(round_half_up(1.245, 1), round_to(1.245, 1))
+compare_round(1.23, 1)
+compare_round(1.25, 1)
+compare_round(1.235, 1)
+compare_round(1.245, 1)
 
 print('2 decimals')
-print(round_half_up(1.234, 2), round_to(1.234, 2))
-print(round_half_up(1.235, 2), round_to(1.235, 2))
-print(round_half_up(1.2344, 2), round_to(1.2344, 2))
-print(round_half_up(1.2345, 2), round_to(1.2345, 2))
+compare_round(1.234, 2)
+compare_round(1.235, 2)
+compare_round(1.2344, 2)
+compare_round(1.2345, 2)
 
 print('negative number, 1 decimal')
-print(round_half_up(-1.23, 1), round_to(-1.23, 1))
-print(round_half_up(-1.25, 1), round_to(-1.25, 1))
-print(round_half_up(-1.235, 1), round_to(-1.235, 1))
-print(round_half_up(-1.245, 1), round_to(-1.245, 1))
+compare_round(-1.23, 1)
+compare_round(-1.25, 1)
+compare_round(-1.235, 1)
+compare_round(-1.245, 1)
 
 print('negative number, 2 decimals')
-print(round_half_up(-1.234, 2), round_to(-1.234, 2))
-print(round_half_up(-1.235, 2), round_to(-1.235, 2))
-print(round_half_up(-1.2344, 2), round_to(-1.2344, 2))
-print(round_half_up(-1.2345, 2), round_to(-1.2345, 2))
+compare_round(-1.234, 2)
+compare_round(-1.235, 2)
+compare_round(-1.2344, 2)
+compare_round(-1.2345, 2)
