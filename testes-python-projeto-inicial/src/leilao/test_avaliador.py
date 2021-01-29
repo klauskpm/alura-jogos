5bd03206331e61dbc610bed3c20b5d94d9abc630
Klaus Kazlauskas <klausmachado@gmail.com>
2021-01-28 22:30:06 -0300

    test(testes): criar método separado para dar setup do yuri

--- testes-python-projeto-inicial/src/leilao/test_avaliador.py
+++ testes-python-projeto-inicial/src/leilao/test_avaliador.py
@@ -4,18 +4,20 @@ from dominio import Usuario, Lance, Leilao, Avaliador
 
 class TestAvaliador(TestCase):
     def setUp(self) -> None:
-        self.gui = Usuario('Gui')
-        self.yuri = Usuario('Yuri')
         self.menor_valor_esperado = 100.0
         self.maior_valor_esperado = 150.0
 
-        self.lance_do_yuri = Lance(self.yuri, self.menor_valor_esperado)
+        self.gui = Usuario('Gui')
         self.lance_do_gui = Lance(self.gui, self.maior_valor_esperado)
 
         self.leilao = Leilao('Celular')
 
+    def setUp_yuri(self) -> None:
+        self.yuri = Usuario('Yuri')
+        self.lance_do_yuri = Lance(self.yuri, self.menor_valor_esperado)
 
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
+        self.setUp_yuri()
         self.leilao.lances.append(self.lance_do_gui)
         self.leilao.lances.append(self.lance_do_yuri)
 
@@ -26,6 +28,7 @@ class TestAvaliador(TestCase):
         self.assertEqual(self.maior_valor_esperado, avaliador.maior_lance)
 
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
+        self.setUp_yuri()
         self.leilao.lances.append(self.lance_do_yuri)
         self.leilao.lances.append(self.lance_do_gui)
 
@@ -45,6 +48,7 @@ class TestAvaliador(TestCase):
         self.assertEqual(self.maior_valor_esperado, avaliador.maior_lance)
 
     def test_deve_retornar_o_maior_e_menor_lance_quando_o_leilao_tiver_tres_lances(self):
+        self.setUp_yuri()
         outro_lance_do_gui = Lance(self.gui, 120.0)
 
         self.leilao.lances.append(self.lance_do_yuri)
