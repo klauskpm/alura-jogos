42951114a456ef6692177c69a00439458af54527
Klaus Kazlauskas <klausmachado@gmail.com>
2021-01-28 22:04:50 -0300

    test(testes): refatorar nomes de testes para nomes descritivos e gigantes

--- testes-python-projeto-inicial/src/leilao/test_avaliador.py
+++ testes-python-projeto-inicial/src/leilao/test_avaliador.py
@@ -3,7 +3,7 @@ from dominio import Usuario, Lance, Leilao, Avaliador
 
 
 class TestAvaliador(TestCase):
-    def test_avalia(self):
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
         menor_valor_esperado = 100.0
@@ -23,7 +23,7 @@ class TestAvaliador(TestCase):
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
 
-    def test_avalia2(self):
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
         menor_valor_esperado = 100.0
