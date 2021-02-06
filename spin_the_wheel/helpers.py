27d3190f69a53aaee20586847f3fd0558fa423df
Klaus Kazlauskas <klausmachado@gmail.com>
2021-02-06 12:13:34 -0300

    refactor(stw): remove método não usado #find_all

--- spin_the_wheel/helpers.py
+++ spin_the_wheel/helpers.py
@@ -17,21 +17,6 @@ def normalize(word):
     return word.strip().upper()
 
 
-def find_all(word, letter):
-    indexes = []
-    current_index = 0
-    can_search = letter in word
-
-    while (can_search):
-        found_index = word.find(letter, current_index)
-        indexes.append(found_index)
-
-        current_index = found_index + 1
-        can_search = letter in word[current_index:]
-
-    return indexes
-
-
 def map_positions(word):
     positions_dict = defaultdict(list)
 
