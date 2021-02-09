5f538b85dceffb76da0468e0003558e3cc761c07
Klaus Kazlauskas <klausmachado@gmail.com>
2021-02-08 22:39:46 -0300

    test(stw): cria mais um teste para pegar palavras randomicas

--- spin_the_wheel/tests/test_SecretWord.py
+++ spin_the_wheel/tests/test_SecretWord.py
@@ -1,6 +1,12 @@
+import pytest
 from random import seed
 
 from spin_the_wheel.words import SecretWord
+
+
+@pytest.fixture(scope='module')
+def path_to_words():
+    return '../words/assets'
 
 
 class Test__get_random_secret_word:
@@ -8,3 +14,9 @@ class Test__get_random_secret_word:
         seed(3)
         word = SecretWord._get_random_secret_word()
         assert word == 'Astro Bot'
+
+    def test_should_get_random_word_from_selected_file(self, path_to_words):
+        file = f'{path_to_words}/fruits.txt'
+        seed(3)
+        word = SecretWord._get_random_secret_word(file)
+        assert word == 'banana'
