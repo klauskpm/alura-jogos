ed89d8a8b3d902fea6e19e3ac0aa2095458f3030
Klaus Kazlauskas <klausmachado@gmail.com>
2021-02-15 18:43:23 -0300

    refactor(stw): pequenas alterações no helpers

--- spin_the_wheel/helpers.py
+++ spin_the_wheel/helpers.py
@@ -1,13 +1,14 @@
 import os
 import time
-from collections import defaultdict
 from sys import argv
 
 DEBUG_MODE = 'debug' in argv[1:]
 
+
 def sleep(seconds):
-    if (not DEBUG_MODE):
+    if not DEBUG_MODE:
         time.sleep(seconds)
+
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
