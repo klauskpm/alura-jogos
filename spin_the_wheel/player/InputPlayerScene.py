589bdd26d3a40f73404015b884cfdbe45938b760
Klaus Kazlauskas <klausmachado@gmail.com>
2021-02-25 18:59:19 -0300

    test(stw): crie testes para o InputPlayerScene

--- spin_the_wheel/player/InputPlayerScene.py
+++ spin_the_wheel/player/InputPlayerScene.py
@@ -1,15 +1,29 @@
+from .exceptions import InvalidName
 from .Player import Player
 
 
 class InputPlayerScene:
     @staticmethod
     def input_players():
-        players_count = int(input("Quantos jogadores serão?"))
+        try:
+            players_count = int(input("Quantos jogadores serão? "))
+        except ValueError:
+            print('Você deve informar um número')
+            return InputPlayerScene.input_players()
+
+        if players_count < 1:
+            print('Deve ter no mínimo 1 jogador(a)')
+            return InputPlayerScene.input_players()
         players = []
+
         while players_count > 0:
-            player_name = input('Qual o nome do jogador?')
-            players.append(Player(player_name))
-            players_count -= 1
+            player_name = input('Qual o nome do jogador? ')
+            try:
+                players.append(Player(player_name))
+                players_count -= 1
+            except InvalidName:
+                print('O jogador(a) deve ter um nome')
+                continue
 
         return tuple(players)
 
--- spin_the_wheel/tests/test_InputPlayerScene.py
+++ spin_the_wheel/tests/test_InputPlayerScene.py
@@ -5,18 +5,12 @@ from spin_the_wheel.player import InputPlayerScene
 
 @pytest.fixture()
 def monkeypatch_input(monkeypatch):
-    def factory(players_count: int, names: [str]):
+    def factory(to_print: [int, str, ...]):
         context = {'count': 0}
 
-        def stub(string):
-            content = None
+        def stub(_):
             count = context.get('count')
-
-            if count == 0:
-                content = players_count
-            else:
-                content = names[count - 1]
-
+            content = to_print[count]
             context['count'] = count + 1
             return content
 
@@ -27,10 +21,39 @@ def monkeypatch_input(monkeypatch):
 
 class Test_input_players:
     def test_should_create_one_player_if_passed_only_1(self, monkeypatch_input):
-        self.count = 0
-
-        monkeypatch_input(1, ['wow'])
+        monkeypatch_input([1, 'wow'])
         players = InputPlayerScene.input_players()
 
+        assert len(players) == 1
         assert players[0].name == 'Wow'
         assert players[0].money == 0
+
+    def test_should_create_the_number_of_players_it_was_passed(self, monkeypatch_input):
+        monkeypatch_input([2, 'wow', 'go'])
+        players = InputPlayerScene.input_players()
+
+        assert len(players) == 2
+        assert players[0].name == 'Wow'
+        assert players[1].name == 'Go'
+
+    def test_should_ask_again_for_the_player_name_if_none_was_given(self, monkeypatch_input):
+        monkeypatch_input([2, 'wow', '', 'go'])
+        players = InputPlayerScene.input_players()
+
+        assert len(players) == 2
+        assert players[0].name == 'Wow'
+        assert players[1].name == 'Go'
+
+    def test_should_ask_again_for_the_number_of_players_if_number_is_smaller_than_1(self, monkeypatch_input):
+        monkeypatch_input([-1, 0, 1, 'wow'])
+        players = InputPlayerScene.input_players()
+
+        assert len(players) == 1
+        assert players[0].name == 'Wow'
+
+    def test_should_ask_again_for_the_number_of_players_if_input_was_not_a_number(self, monkeypatch_input):
+        monkeypatch_input(['a', '', 'vozes', 1, 'wow'])
+        players = InputPlayerScene.input_players()
+
+        assert len(players) == 1
+        assert players[0].name == 'Wow'
