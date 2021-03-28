bc944f4fc2b0b14bfcc0a30bfa9c6ddfaa4451f5
Klaus Kazlauskas <klausmachado@gmail.com>
2021-03-28 15:42:21 -0300

    feat(stw): adicionar multiplas rodadas de jogo

--- spin_the_wheel/Game.py
+++ spin_the_wheel/Game.py
@@ -1,7 +1,7 @@
 from random import randrange
 from time import sleep
 
-from player import InputPlayerScene
+from player import InputPlayerScene, Player
 from Round import Round
 from helpers import sleep, clear
 from words import SecretWord
@@ -17,14 +17,28 @@ class Game:
     def start(self):
         self._print_opening_message()
 
-        theme = self.draw_theme()
-        secret_world = SecretWord(theme=theme)
         players = InputPlayerScene.input_players()
-        Round(secret_world, theme, players).run()
+        players = self._run_rounds(3, players)
 
         self._end_game(players)
 
-    def draw_theme(self):
+    def _run_rounds(self, number_of_rounds: int, players: [Player, ...]):
+        clear()
+        print(f'Serão {number_of_rounds} rodadas para decidir quem vai ganhar')
+        sleep(2.5)
+        print('Ganha quem tiver mais dinheiro')
+        sleep(2.5)
+
+        current_round = 1
+        while current_round <= number_of_rounds:
+            theme = self._draw_theme()
+            secret_world = SecretWord(theme=theme, word='klaus')
+            Round(secret_world, theme, players).run()
+            current_round += 1
+
+        return players
+
+    def _draw_theme(self):
         themes = ['fruits', 'video_games']
         return themes[randrange(0, len(themes))]
 
